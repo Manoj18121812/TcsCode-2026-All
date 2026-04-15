f34812dff9e989ad4b0cfeff993fa7b9d13cc792
Manoj Gavhane <131341403+Manoj18121812@users.noreply.github.com>
2026-04-15 20:46:33 +0530

    ok

---
+++ src/main/java/AmountDiscount.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class AmountDiscount {
+
+    public static void main(String[]args){
+        
+        Scanner sc = new Scanner(System.in);
+        String manoj=sc.nextLine();
+        int amount = Integer.parseInt(manoj);
+
+        double discount;
+
+        if(amount>=5000){
+            discount = amount-(amount*0.20);
+        }else if(amount>=2000){
+            discount = amount-(amount*0.15);
+        }else{
+            discount=amount;
+        }
+
+        System.out.println((int)discount);
+        System.out.println(Math.round(discount));
+    }
+}
